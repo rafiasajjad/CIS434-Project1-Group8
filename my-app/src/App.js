1c87da4dfce78769310d7787e3b10187821c756c
hamza <hamza@rotundascitech.com>
2023-02-03 12:10:23 -0500

    Modified Src for csu link

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,26 @@
 import test1 from "./engineering.jpg";
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "new title"
+ }, []);
   return (
+    
     <div className="App">
       <header className="App-header">
       <img src={test1} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Access CSU Washkewicz Website below
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://engineering.csuohio.edu/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about CSU
         </a>
       </header>
     </div>
