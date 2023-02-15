fd6c6189ef5e7b0eb96130047c1ee443f15d712f
Hamzas61 <hamza.srhiri@gmail.com>
2023-02-15 15:25:56 -0500

    Added transaction name & display it under transaction history

--- my-app/src/transaction.js
+++ my-app/src/transaction.js
@@ -1,19 +1,34 @@
 //Getting both values of transaction and balance
 const balanceElement = document.getElementById("balance");
 const transactionElement = document.getElementById("transaction");
-const transactionHistory = document.getElementById("transaction-history");
+const transactionNameElement = document.getElementById("transaction-name");
 //intiating balance to 0
 let balance = 0;
+let transactionHistory = [];
+
 //updating the balance after pressing the submit button
-  const form = document.querySelector("form");
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-      const transaction = parseFloat(transactionElement.value);
-      balance += transaction;
-      balanceElement.textContent = balance;
-      //Updating the transaction box to be empty after the submision 
-      transactionElement.value = "";
-      const newTransaction = document.createElement("li");
-      newTransaction.textContent = `$${transaction}`;
-      transactionHistory.appendChild(newTransaction);
+const form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const transaction = parseFloat(transactionElement.value);
+  const transactionName = transactionNameElement.value;
+  balance += transaction;
+  balanceElement.textContent = balance;
+  transactionHistory.push({ name: transactionName, amount: transaction });
+  displayTransactionHistory();
+  //Updating the transaction box to be empty after the submision
+  transactionElement.value = "";
+  transactionNameElement.value = "";
+});
+
+function displayTransactionHistory() {
+  const transactionList = document.getElementById("transaction-list");
+  transactionList.innerHTML = "";
+  transactionHistory.forEach(transaction => {
+    const transactionItem = document.createElement("li");
+    const transactionName = transaction.name ;
+    const transactionAmount = transaction.amount.toFixed(2);
+    transactionItem.textContent = `${transactionName}: $${transactionAmount}`;
+    transactionList.appendChild(transactionItem);
   });
+}
