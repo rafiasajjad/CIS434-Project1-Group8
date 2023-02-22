ac74db553bea525b3d6da7efdad12e4e59908df7
Hamzas61 <hamza.srhiri@gmail.com>
2023-02-22 00:18:19 -0500

    Added local storage to save history, and changed the UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { updateBalance } from './transaction.js';
+import './transaction.js';
 
 function App() {
-  const [balance, setBalance] = useState(0);
-  const [transactions, setTransactions] = useState([]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>Current balance: <span id="balance">{balance}</span></p>
-        <form>
-          <input type="text" id="transaction" />
-          <button
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              const transaction = parseFloat(document.getElementById("transaction").value);
-              updateBalance(transactions, setTransactions, balance, setBalance, transaction);
-            }}
-          >
-            Submit
-          </button>
-        </form>
       </header>
     </div>
   );
--- my-app/src/transaction.js
+++ my-app/src/transaction.js
@@ -1,14 +1,25 @@
-//Getting both values of transaction and balance
+// Getting both values of transaction and balance
 const balanceElement = document.getElementById("balance");
-const transactionElement = document.getElementById("transaction");
+const transactionElement = document.getElementById("transaction-amount");
 const transactionNameElement = document.getElementById("transaction-name");
-//intiating balance to 0
+
+// Initializing balance to 0 and transaction history to empty array
 let balance = 0;
 let transactionHistory = [];
 
-//updating the balance after pressing the submit button
+// Retrieve transaction history from local storage if available
+if (localStorage.getItem("transactionData")) {
+  transactionHistory = JSON.parse(localStorage.getItem("transactionData"));
+  balance = transactionHistory.reduce((name, transaction) => {
+    return name + transaction.amount;
+  }, 0);
+  balanceElement.textContent = balance.toFixed(2);
+  displayTransactionHistory();
+}
+
+// Updating the balance after pressing the submit button
 const form = document.querySelector("form");
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
   const transaction = parseFloat(transactionElement.value);
   const transactionName = transactionNameElement.value;
@@ -16,10 +27,12 @@ form.addEventListener("submit", function(event) {
     alert("Transaction amount exceeds current balance. Transaction canceled.");
   } else {
     balance += transaction;
-    balanceElement.textContent = balance;
+    balanceElement.textContent = balance.toFixed(2);
     transactionHistory.push({ name: transactionName, amount: transaction });
     displayTransactionHistory();
-    //Updating the transaction box to be empty after the submision
+    // Store transaction history in local storage
+    localStorage.setItem("transactionData", JSON.stringify(transactionHistory));
+    // Updating the transaction box to be empty after the submission
     transactionElement.value = "";
     transactionNameElement.value = "";
   }
@@ -28,11 +41,30 @@ form.addEventListener("submit", function(event) {
 function displayTransactionHistory() {
   const transactionList = document.getElementById("transaction-list");
   transactionList.innerHTML = "";
-  transactionHistory.slice().reverse().forEach(transaction => { 
-    const transactionItem = document.createElement("li");
-    const transactionName = transaction.name ;
+  transactionHistory.slice().reverse().forEach((transaction) => {
+    const row = document.createElement("tr");
+    const nameCell = document.createElement("th");
+    const amountCell = document.createElement("th");
+    const transactionName = transaction.name || "Unnamed Transaction";
     const transactionAmount = transaction.amount.toFixed(2);
-    transactionItem.textContent = `${transactionName}: $${transactionAmount}`;
-    transactionList.appendChild(transactionItem);
+    nameCell.textContent = transactionName;
+    amountCell.textContent = `$${transactionAmount}`;
+    if (transactionAmount >= 0) {
+      amountCell.classList.add("positive");
+    } else {
+      amountCell.classList.add("negative");
+    }
+    row.appendChild(nameCell);
+    row.appendChild(amountCell);
+    transactionList.appendChild(row);
   });
 }
+//Clearing the transaction history in local storage
+const clearHistoryButton = document.getElementById("clear-history");
+clearHistoryButton.addEventListener("click", function() {
+  localStorage.removeItem("transactionData");
+  transactionHistory = [];
+  balance = 0;
+  balanceElement.textContent = balance.toFixed(2);
+  displayTransactionHistory();
+});
